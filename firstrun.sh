c23a4c15b1b04baa760f89361cf6282e9b8c4e64
Dockhorn <dockhorn@hft-leipzig.de>
2019-06-11 18:26:51 +0200

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 name="pi$(($RANDOM%1000))" && echo $name && hostname -b "$name" && echo $name > /etc/hostname
-#sudo apt-get update
+sudo apt-get update
 #sudo apt-get -y upgrade
-#sudo apt install -y git zsh htop i2c-tools libi2c-dev python-smbus
+sudo apt install -y git zsh htop i2c-tools libi2c-dev python-smbus
