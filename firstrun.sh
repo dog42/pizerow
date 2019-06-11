fdd471d4af012d49e6b345afec5c1b68dce1bc5e
dog42 <dockhorn@hft-leipzig.de>
2019-06-11 17:27:47 +0200

    ...

--- firstrun.sh
+++ firstrun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo hostname -b "pi$(($RANDOM%1000))"
-sudo apt-get update
+#sudo apt-get update
 #sudo apt-get -y upgrade
-sudo apt install -y git zsh htop i2c-tools libi2c-dev python-smbus
+#sudo apt install -y git zsh htop i2c-tools libi2c-dev python-smbus
