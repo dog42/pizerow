b15e14b8f2f1d978fe7ab311c98c7ec1e354173a
Dockhorn <dockhorn@hft-leipzig.de>
2019-09-10 15:05:19 +0200

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#name="pi$(ip -o addr show dev "eth0" | awk '$3 == "inet" {print $4}' | sed -r 's!/.*!!; s!.*\.!!')"
-name="pi$(($RANDOM%1000))" && echo $name && hostname -b "$name" && echo $name > /etc/hostname
+name="pi$(ip -o addr show dev "eth0" | awk '$3 == "inet" {print $4}' | sed -r 's!/.*!!; s!.*\.!!')" && echo $name && hostname -b "$name" && echo $name > /etc/hostname
 sudo apt-get update
 #sudo apt-get -y upgrade
 sudo apt install -y git zsh htop i2c-tools libi2c-dev python-smbus
